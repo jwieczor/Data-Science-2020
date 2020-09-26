0e4937753a5a562430ca150ca9c0bd6bc0ec8468
jwieczor <s1710092@sms.ed.ac.uk>
2020-09-26 19:25:24 +0100

    Jakub added a new file

---
+++ manuscripts/error403_explained.R
@@ -0,0 +1,7 @@
+# This is an explanation to an error I have encountered myself when trying to push for the first time
+# error403 is related to credentials
+# what to do (Mac users):
+# In Finder, search for the Keychain Access app.
+# In Keychain Access, search for github.com.
+# Find the "internet password" entry for github.com.
+# Edit or delete the entry accordingly.
